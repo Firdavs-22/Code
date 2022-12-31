628c0417f39b5f3020e57c2f679fc85deb6e2fcd
stoicist <hm1544727@gmail.com>
2022-12-31 17:51:47 +0500

    new java files added

---
+++ Nur_Islom/java/code_with_mosh/src/PartOne/MortgageCalc2.java
@@ -0,0 +1,45 @@
+package PartOne;
+import java.util.Scanner;
+import java.text.NumberFormat;
+
+public class MortgageCalc2 {
+    public static void main(String[] args) {
+
+        int principal = (int) readNumber("Principal: ", 1_000, 1_000_000);
+
+        float annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 100);
+
+        byte period = (byte) readNumber("Period (Years): ", 1, 30);
+
+        double mortgage = calculateMortgage(principal, annualInterest, period);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Monthly payment: " + mortgageFormatted);
+        System.out.println("Payment Schedule:");
+        while (principal > 0) {
+            principal -= mortgage;
+            mortgage = calculateMortgage(principal, annualInterest, period);
+            System.out.println(NumberFormat.getCurrencyInstance().format(principal));
+        }
+    }
+    public static double readNumber(String prompt, double min, double max){
+        Scanner scan = new Scanner(System.in);
+        double value;
+        while(true){
+            System.out.print(prompt);
+            value = scan.nextInt();
+            if (value >= min && value <= max) break;
+            System.out.println("Please Enter a value between " + min + " and " + max + ".");
+        }
+        return value;
+    }
+    public static double calculateMortgage(int principal, float annualInterest, float period){
+        byte MONTHS_IN_YEAR = 12;
+        byte PERCENT = 100;
+        short numberOfPayments = (short) (period * MONTHS_IN_YEAR);
+        float monthlyInterest = annualInterest / MONTHS_IN_YEAR / PERCENT;
+        double mortgage = principal
+                * (Math.pow(1 + monthlyInterest, numberOfPayments) * monthlyInterest)
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+        return mortgage;
+    }
+}
