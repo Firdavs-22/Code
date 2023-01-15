4bbdeeb1aa4fa6ab98c8ee7c96010e7b8e28984e
stoicist <hm1544727@gmail.com>
2023-01-15 19:56:00 +0500

    new nodejs files

---
+++ Nur_Islom/nodejs/design_patterns/arrowFunction.js
@@ -0,0 +1,13 @@
+function DelayedGreeter(name) {
+    this.name = name;
+}
+/*DelayedGreeter.prototype.greet = function() {
+    setTimeout( (function cb() {
+        console.log('Hello' + this.name);
+    }).bind(this), 500);
+};*/
+DelayedGreeter.prototype.greet = function() {
+    setTimeout( () => console.log('Hello' + this.name), 500);
+};
+const greeter = new DelayedGreeter('World');
+greeter.greet();
