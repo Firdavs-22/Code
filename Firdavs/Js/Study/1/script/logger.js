a21a132ddcf7e9f86869ecbfba322198efd9b798
stoicist <hm1544727@gmail.com>
2023-01-25 07:15:53 +0500

    js started

--- Firdavs/Js/Study/1/script/logger.js
+++ Firdavs/Js/Study/1/script/logger.js
@@ -1,10 +1,10 @@
-export let logger = text => {
+export let yoz = text => {
     let line = true
     if(typeof text == 'object'){
         for (let key in text) {
             if (typeof text[key] == 'object'){
                 line = false
-                logger(text[key])
+                yoz(text[key])
             } else{
                 write(key+': '+text[key]);
             }
